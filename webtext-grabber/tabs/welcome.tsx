1f407536168b2275c4ac688d8cde05b274cbba22
Dean Cirielli <dcirielli5@gmail.com>
2024-03-25 19:43:48 +0000

    Disable pages, add Welcome page

---
+++ webtext-grabber/tabs/welcome.tsx
@@ -0,0 +1,37 @@
+import { usePort } from "@plasmohq/messaging/hook";
+const pkg = require("../package.json")
+const { displayName: name, version, author, dependencies, description } = pkg;
+
+type RequestBody = {
+  hello: string
+}
+
+type ResponseBody = {
+  message: string
+}
+
+function WelcomeTab() {
+  const mailPort = usePort<RequestBody, ResponseBody>("mail")
+
+  return (
+    <div>
+      {/* {mailPort.data?.message}
+      <button
+        onClick={async () => {
+          mailPort.send({
+            hello: "world"
+          })
+        }}>
+        Send Port
+      </button> */}
+      <h1>Welcome to {name}!</h1>
+      <div>This is {name} version {version}, {description[0].toLowerCase()}{description.slice(1)} written by {author}.</div>
+      <details>
+        <summary>Dependencies</summary>
+          {Object.entries(dependencies).map(([n, v], _i) => (<div>{n}@{v}</div>))}
+      </details>
+    </div>
+  )
+}
+
+export default WelcomeTab
