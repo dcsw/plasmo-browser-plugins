0fc7f8eeb8d57cf311814a4eac93465b89d22a2e
dcsw <dcirielli5@gmail.com>
2025-01-13 13:22:10 +0000

    WIP -- collapse into 1 docx section

--- webtext-grabber/DocxGenerator.ts
+++ webtext-grabber/DocxGenerator.ts
@@ -1,11 +1,11 @@
-import { Document, Packer, Paragraph, TextRun, ExternalHyperlink, ImageRun, HeadingLevel, BorderStyle, AlignmentType } from 'docx';
+import {
+  Document, Packer, Paragraph, TextRun, ExternalHyperlink, ImageRun, HeadingLevel, BorderStyle, AlignmentType,
+  sectionMarginDefaults, sectionPageSizeDefaults, PageSize, convertInchesToTwip
+} from 'docx';
 
 export const makeDoc = async (sel: string) => {
-  const o = {
-    sections: [{
-      properties: {},
-      children: []
-    }]
+  let o = {
+    sections: []
   }
 
   const items = Array.from(document.querySelectorAll(sel + ' > .item'))
@@ -13,92 +13,71 @@ export const makeDoc = async (sel: string) => {
   const linkUrls = items.map(i => i.querySelector('a').href)
   const linkTexts = items.map(i => i.querySelector('a').textContent)
   const screenShotImgUrls = items.map(i => i.querySelector('img').src)
+  const heights = items.map(i => i.querySelector('img').clientHeight)
+  const widths = items.map(i => i.querySelector('img').clientWidth)
 
   // const level: HeadingLevel = HeadingLevel.HEADING_1
-  const level = HeadingLevel.HEADING_1
+  const level = HeadingLevel.HEADING_2
   // const alignment: AlignmentType = AlignmentType.LEFT
   const alignment = AlignmentType.LEFT
 
-  for (let i = 0; i < items.length; i++) {
-    o.sections.push({
-      properties: {},
-      children: [
-        new Paragraph({
-          text: titles[i],
-          heading: level
-        }),
-        new Paragraph({
-          alignment: alignment,
-          children: [
-            new ExternalHyperlink({
-              children: [
-                new TextRun({
-                  text: linkTexts[i],
-                  style: "Hyperlink"
-                })
-              ],
-              link: linkUrls[i]
-            }),
-            // new ImageRun({
-            //   data: Buffer.from(screenShotImgUrls[i].split(',')[1], 'base64'),
-            //   // transformation: {
-            //   //   width: 200,
-            //   //   height: 200,
-            //   // },
-            // })
-          ]
-        }),
-        new Paragraph("No border!"),
-        new Paragraph({
-          text: "I have borders on my top and bottom sides!",
-          border: {
-            top: {
-              color: "auto",
-              space: 1,
-              style: BorderStyle.SINGLE,
-              size: 6,
-            },
-            bottom: {
-              color: "auto",
-              space: 1,
-              style: BorderStyle.SINGLE,
-              size: 6,
-            },
-          },
-        }),
-        new Paragraph({
-          text: "",
-          border: {
-            top: {
-              color: "auto",
-              space: 1,
-              style: BorderStyle.SINGLE,
-              size: 6,
-            },
-          },
-        }),
-        new Paragraph({
-          children: [
-            new TextRun({
-              text: "This will ",
-            }),
-            new TextRun({
-              text: "have a border.",
-              border: {
-                color: "auto",
-                space: 1,
-                style: BorderStyle.SINGLE,
-                size: 6,
-              },
-            }),
-            new TextRun({
-              text: " This will not.",
-            }),
-          ],
-        }),
-      ]
-    })
-    const doc = await new Document(o)
-    return await Packer.toBuffer(doc)
+  // Calculate available width (in twips)
+  // const pageWidth = convertInchesToTwip(sectionPageSizeDefaults.WIDTH);
+  // const pageWidth = sectionPageSizeDefaults.WIDTH;
+  // const margins = convertInchesToTwip(2); // 1-inch margins on each side
+  // const availableWidth = pageWidth - margins;
+  // const availableWidth = sectionPageSizeDefaults.WIDTH - sectionMarginDefaults.LEFT - sectionMarginDefaults.RIGHT
+
+  const myPageSize = {
+    width: convertInchesToTwip(8.5),
+    height: convertInchesToTwip(11),
   }
+  const myMarginSizes = {
+    top: convertInchesToTwip(1),
+    right: convertInchesToTwip(1),
+    bottom: convertInchesToTwip(1),
+    left: convertInchesToTwip(1),
+  }
+  const availableWidth = myPageSize.width - myMarginSizes.left - myMarginSizes.right
+  o.sections.push({
+    properties: {
+      page: {
+        size: myPageSize,
+        margin: myMarginSizes,
+      },
+    },
+    children: []
+  })
+  for (let i = 0; i < items.length; i++) {
+    o.sections[0].children.push(
+      new Paragraph({
+        text: titles[i],
+        heading: level
+      }))
+    o.sections[0].children.push(
+      new Paragraph({
+        alignment: alignment,
+        children: [
+          new ExternalHyperlink({
+            children: [
+              new TextRun({
+                text: linkTexts[i],
+                style: "Hyperlink"
+              })
+            ],
+            link: linkUrls[i]
+          }),
+          new ImageRun({
+            data: Buffer.from(screenShotImgUrls[i].split(',')[1], 'base64'),
+            transformation: {
+              // width: availableWidth, // widths[i],
+              // height: availableWidth * heights[i] / widths[i] // preserve aspect ratio
+              width: widths[i],
+              height: heights[i]
+            }
+          })]
+      }))
+  }
+  const doc = new Document(o)
+  return await Packer.toBuffer(doc)
 }
